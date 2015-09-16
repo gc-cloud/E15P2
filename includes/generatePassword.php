b05395b639a823b774159eb6328c89e6012ba6c9
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-09-15 21:31:18 -0500

    Sticky forms and uppercase checbox OK

--- includes/generatePassword.php
+++ includes/generatePassword.php
@@ -11,42 +11,48 @@ include("data.php");
 characters, numbers and words*/
 $password = array();
 
-/*Get user selected values for number of words, numbers and special characters.
-Assummes that the origin form is using the POST method.*/
-$wordCount = $_POST['wordCount'];
-$numberCount = $_POST['numberCount'];
-$specialCharacterCount = (int)$_POST['specialCharacterCount'];
+/* Define a function to set the value of $variable to that of the
+$_POST[$variable] to a default if $_POST[$variable] is empty.  This function is
+meant to be used to support sticky forms that keep the values selected by the user */
+
+/* If set, get user defined values for number of words, numbers and
+special characters, otherwise set defaults.*/
+isset($_POST['wordCount'])? $wordCount = $_POST['wordCount']:$wordCount= 4;
+isset($_POST['numberCount'])? $numberCount = $_POST['numberCount']:$numberCount= 1;
+isset($_POST['specialCharacterCount'])? $specialCharacterCount = $_POST['specialCharacterCount']:$specialCharacterCount= 1;
+isset($_POST['uppercase'])? $uppercase = $_POST['uppercase']:$uppercase= false;
+echo $uppercase;
 
 /* Create a generic function to select random elements form a source
 (for example: specialCharacters,numbers,wordCorpus) and then add the
 elements to a destination (arguably the password array). This function handles
 the special cases of 1 element (not an array) and 0 elements (ignore)*/
 
-function addElements ($source, &$destination,$count){
+function addElements ($source, &$destination,$count, $upperCase){
     $randomKeys  = array_rand($source, $count);
     if (count($randomKeys) == 1){
       /* If only one element, array_rand returns an integer, not an array */
-       $destination[] = $source[$randomKeys];
+      ($upperCase)? $destination[] = ucfirst($source[$randomKeys]) : $destination[] = $source[$randomKeys];
+
     } elseif (count($randomKeys) == 0){
       /* If no elements, we do nothing */
     } else {
       /* If elements <> 0,1 then we have an array of results */
       foreach ($randomKeys as $randomKey) {
-           $destination[] = $source[$randomKey];
+          ($upperCase)?  $destination[] = ucfirst($source[$randomKey]) :$destination[] = $source[$randomKey];
        }
     }
 }
 
 /* Call the addElements function to select words, numbers and special characters */
-addElements ($wordCorpus, $password,$wordCount);
-addElements ($numbers, $password,$numberCount);
-addElements ($specialCharacters, $password,$specialCharacterCount);
+addElements ($wordCorpus, $password,$wordCount, $uppercase);
+addElements ($numbers, $password,$numberCount, $uppercase);
+addElements ($specialCharacters, $password,$specialCharacterCount, $uppercase);
 
 
 /* At this point, the $password array has a list of randomly selected
 specialCharacters, numbers and words. However, they are in the order that the
 elements were selected.  We use the shuffle function to scramble the elements*/
-  echo "Words = $wordCount, Special Characters = $specialCharacterCount, Numbers = $numberCount <br>";
   shuffle($password);
 
 /* This function outputs the proposed password to the terminal. It is designed to
@@ -56,7 +62,4 @@ be called by the form using it */
         echo $value ;
       }
   }
-
-/*printPassword($password);*/
-
 ?>
--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   </head>
   <body>
   	<?php include("includes/header.php"); ?>
+    <?php
+    {
+      include("includes/generatePassword.php");
+    } ?>
           <div class="container">
           <div class="row mainContent" title="Dynamic Web Apps Portfolio">
               <div class="twelve columns">
@@ -15,38 +19,44 @@
                     words, numbers, and special characters. <a href='http://xkcd.com/936/'>(reference : xkcd archive)</a></p>
                     <form action="index.php" method="post" class="options, parameters">
                       <!-- run server side scritps only after form is posted-->
-                      <?php  if ($_SERVER['REQUEST_METHOD']=='POST')
+                      <!--?php  if ($_SERVER['REQUEST_METHOD']=='POST')
                       {
                         include("includes/generatePassword.php");
-                      } ?>
+                      } ?-->
+
                       <h3> Select Password Requirements</h3>
                         <label for='wordCount'># of Words</label>
                         <select name='wordCount' id='wordCount'>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4" selected='selected'>4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
+                            <option <?php if($wordCount=="0") echo 'selected="selected"'; ?> value="0">0</option>
+                            <option <?php if($wordCount=="1") echo 'selected="selected"'; ?> value="1">1</option>
+                            <option <?php if($wordCount=="2") echo 'selected="selected"'; ?> value="2">2</option>
+                            <option <?php if($wordCount=="3") echo 'selected="selected"'; ?> value="3">3</option>
+                            <option <?php if($wordCount=="4") echo 'selected="selected"'; ?> value="4">4</option>
+                            <option <?php if($wordCount=="5") echo 'selected="selected"'; ?> value="5">5</option>
+                            <option <?php if($wordCount=="6") echo 'selected="selected"'; ?> value="6">6</option>
                         </select>
                         <label for='numberCount'># of Numbers</label>
                           <select name='numberCount' id='numberCount'>
-                              <option value="0">0</option>
-                              <option value="1" selected='selected'>1</option>
-                              <option value="2">2</option>
-                              <option value="3">3</option>
-                              <option value="4">4</option>
-                              <option value="5">5</option>
+                            <option <?php if($numberCount=="0") echo 'selected="selected"'; ?> value="0">0</option>
+                            <option <?php if($numberCount=="1") echo 'selected="selected"'; ?> value="1">1</option>
+                            <option <?php if($numberCount=="2") echo 'selected="selected"'; ?> value="2">2</option>
+                            <option <?php if($numberCount=="3") echo 'selected="selected"'; ?> value="3">3</option>
+                            <option <?php if($numberCount=="4") echo 'selected="selected"'; ?> value="4">4</option>
+                            <option <?php if($numberCount=="5") echo 'selected="selected"'; ?> value="5">5</option>
+                            <option <?php if($numberCount=="6") echo 'selected="selected"'; ?> value="6">6</option>
                           </select>
                           <label for='specialCharacterCount'># of Special Characters</label>
                           <select name='specialCharacterCount' id='specialCharacterCount'>
-                              <option value="0">0</option>
-                              <option value="1" selected='selected'>1</option>
-                              <option value="2">2</option>
-                              <option value="3">3</option>
-                              <option value="4">4</option>
-                              <option value="5">5</option>
+                            <option <?php if($specialCharacterCount=="0") echo 'selected="selected"'; ?> value="0">0</option>
+                            <option <?php if($specialCharacterCount=="1") echo 'selected="selected"'; ?> value="1">1</option>
+                            <option <?php if($specialCharacterCount=="2") echo 'selected="selected"'; ?> value="2">2</option>
+                            <option <?php if($specialCharacterCount=="3") echo 'selected="selected"'; ?> value="3">3</option>
+                            <option <?php if($specialCharacterCount=="4") echo 'selected="selected"'; ?> value="4">4</option>
+                            <option <?php if($specialCharacterCount=="5") echo 'selected="selected"'; ?> value="5">5</option>
+                            <option <?php if($specialCharacterCount=="6") echo 'selected="selected"'; ?> value="6">6</option>
                           </select>
+                          <!--input type="checkbox" name="uppercases" value="true" checked> uppercase<br-->
+                          <input type="checkbox" name="uppercase" <?php if($uppercase) echo 'value="true" checked'; ?> > uppercase<br>
                         </br>
                           <input type='submit' class='btn btn-default' value='New Password'> <br/>
                       <h2>Proposed password</h2>
