bdda1fe793323a5e8974955ad6a2f5460c5b931e
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-09-16 10:23:16 -0500

    Improved layout

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
     <header>
       <div class="container">
       <div class="projectName">
-        <h1 >xckd</h1>
+        <h1 >xkcd</h1>
         <h2 > Password Generator</h2>
       </div>
       <img src="images/password.png"  alt="xkcd_password" class="logo">
