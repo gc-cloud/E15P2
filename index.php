74f4481489346f98aa20ba7a9ce061b050b50c39
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-09-19 21:20:56 -0500

    Checkbox logic

--- index.php
+++ index.php
@@ -33,10 +33,10 @@
             </select>
           </div>
           <div class="col-sm-3">
-            <label for="camelCase" >Use Camel Case</label>
+            <label for="camelCase">Use Camel Case</label>
           </div>
           <div class="col-sm-3">
-            <input type="checkbox" name="camelCase" <?php if($camelCase) echo ' value="true" checked '; ?>>
+            <input type="checkbox" name="camelCase" <?php if($camelCase) {echo ' value="true" checked ';} else {echo 'value="false"';} ?>>
           </div>
         </div> <!-- end row -->
         <div class="row">
